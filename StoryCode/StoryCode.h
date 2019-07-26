574430d25bb257d1646a1f495bff1a845ed90f72
Anton Sergeev <anton.sergeev@corp.vk.com>
2019-07-26 21:18:58 +0300

    Initial Commit

---
+++ StoryCode/StoryCode.h
@@ -0,0 +1,19 @@
+//
+//  StoryCode.h
+//  StoryCode
+//
+//  Created by Антон Сергеев on 26/07/2019.
+//  Copyright © 2019 antonsergeev88. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StoryCode.
+FOUNDATION_EXPORT double StoryCodeVersionNumber;
+
+//! Project version string for StoryCode.
+FOUNDATION_EXPORT const unsigned char StoryCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StoryCode/PublicHeader.h>
+
+
